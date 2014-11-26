1e509cada3917a5b4d53b863f7790d624f82689a
David Göransson <david.goransson90@gmail.com>
2014-11-26 02:32:16 +0100

    Add oh-my-zsh aliases

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,9 @@
+alias ealiases='vim ~/.oh-my-zsh/custom/aliases.zsh'
+alias ea='vim ~/.config/awesome/rc.lua'
+alias h='history'
+alias open='xdg-open'
+alias o='xdg-open'
+alias cl='clear'
+alias fml='sudo $(fc -nl -1)'
+alias saywhat='whatis "$(fc -nl -1 | awk "{print \$NF}")"'
+
