239fadb346025a9190adaa3cd2ad44c08a07325d
David Göransson <david.goransson90@gmail.com>
2014-12-01 09:11:55 +0100

    Add 2 wp and fix wallpaper script

--- .wallpaper.sh
+++ .wallpaper.sh
@@ -1,5 +1,5 @@
 while true; do
-    find ~/.wallpapers -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
+    find ~/.wallpapers/ -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
         shuf -n1 -z | xargs -0 feh --bg-fill
     sleep 15m
 done
