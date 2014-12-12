8ff13750b747e647d9989f0837c9b5f6229022e6
David Göransson <david.goransson90@gmail.com>
2014-12-12 02:49:09 +0100

    Add mouse support for irc

--- .bin/irc-start.sh
+++ .bin/irc-start.sh
@@ -1,7 +1,7 @@
 success=false;
 while [ "$success" != "true" ]; do
     if ping -c1 -q ping.sunet.se > /dev/null; then
-        urxvt -name irc -e mosh irc &
+        urxvt -name irc -e "irc"
         success=true
     else
         sleep 0.5
