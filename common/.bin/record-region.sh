b35173586f5e3415b00ae440af66cc1b6fec3feb
David Göransson <david.goransson90@gmail.com>
2017-08-10 15:44:11 +0200

    Add record-region script!

---
+++ common/.bin/record-region.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Delay before starting
+DELAY=5
+
+# Duration and output file
+if [ $# -gt 0 ]; then
+    D="--duration=$@"
+else
+    echo Default recording duration 10s to /tmp/recorded.gif
+    D="--duration=10 /tmp/recorded.gif"
+fi
+
+# xrectsel from https://github.com/lolilolicon/xrectsel
+ARGUMENTS=$(xrectsel "--x=%x --y=%y --width=%w --height=%h") || exit -1
+
+echo Delaying $DELAY seconds. After that, byzanz will start
+for (( i=$DELAY; i>0; --i )) ; do
+    echo $i
+    sleep 1
+done
+
+byzanz-record --verbose --delay=0 ${ARGUMENTS} $D
