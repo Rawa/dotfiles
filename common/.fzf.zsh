cb6c8382f1b24a7c87a0d6afde2fb50bdd74a554
David Göransson <david.goransson@mullvad.net>
2025-11-05 17:29:06 +0100

    Add fzf config

---
+++ common/.fzf.zsh
@@ -0,0 +1,5 @@
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#eaeaea,bg:#282c34,hl:#70c0b1 --color=fg+:#ffffff,bg+:#1e2129,hl+:#81ebd8 --color=info:#afaf87,prompt:#b9c94d,pointer:#b9c94b --color=marker:#eaeaea,spinner:#d54e53,header:#b9c94b'
+
+# FZF
+export FZF_DEFAULT_COMMAND='rg --files --hidden --no-ignore-vcs --follow'
+export FZF_CTRL_T_COMMAND='rg --files --hidden --follow'
